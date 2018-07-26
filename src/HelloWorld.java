a220591978398b22ca0ba406a914680914659776
1 <1@a>
2018-07-26 17:43:38 +0900

    다시 시도

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args ) {
-		System.out.println("이문수 만세");
-		System.out.println("뭐라는 거니?");
+		System.out.println("Hello World");
 		System.out.println("test");
 	}
 }
